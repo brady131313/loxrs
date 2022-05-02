5c5baafc3c5a8ea1778b4c5057f6101dfdf99875
Brady Pierce <bradypierce13@gmail.com>
2022-05-01 20:25:24 -0700

    Add readline repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::error::Error;
+
+use rustyline::{Editor, error::ReadlineError};
+
+const HISTORY: &'static str = ".lox_history.txt";
+
+fn repl() -> Result<(), Box<dyn Error>> {
+    let mut rl = Editor::<()>::new();
+    rl.load_history(HISTORY).unwrap_or(());
+
+    loop {
+        let readline = rl.readline("lox> ");
+        match readline {
+            Ok(line) => {
+                println!("{line}");
+                rl.add_history_entry(line.as_str());
+            },
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => break,
+            Err(err) => {
+                eprintln!("Error: {err:?}");
+                break;
+            }
+        }
+    }
+
+    rl.save_history(HISTORY)?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    repl().expect("Repl error")
 }
